da28101d756845adce79114b72e9db72f8f01489
ammar <ammarnazmi44@gmail.com>
2024-03-15 07:39:38 +0800

    update-navbar

--- frontend/src/views/components/Navbar/Navbar.jsx
+++ frontend/src/views/components/Navbar/Navbar.jsx
@@ -13,24 +13,36 @@ export default function Navbar() {
   return (
     <>
       <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
-      <nav className="nav-wrapper">
-        <div className="nav-content">
-          <img className="logo" src="/src/assets/8334 [Converted].png"/>
-          <ul>
-            <li>
-              <a className="menu-item">Events</a>
-            </li>
-            <li>
-              <a className="menu-item">Results</a>
-            </li>
-            <li>
-              <a className="menu-item">About Us</a>
-            </li>
-            <li>
-              <a className="menu-item">Contact Us</a>
-            </li>
-            <Button className="register-button">Register</Button>
-          </ul>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand align-items-center" href="#">
+            <img className="logo" src="/src/assets/8334 [Converted].png" />
+          </a>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+              <li class="nav-item mx-4">
+                <a class="menu-item" href="#">
+                  <a className="menu-item">Events</a>
+                </a>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="menu-item " href="#">
+                  Results
+                </a>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="menu-item " href="#">
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="menu-item " href="#">
+                  Contact Us
+                </a>
+              </li>
+              <Button className="register-button">Register</Button>
+            </ul>
+          </div>
           <button className="menu-btn" onClick={toggleMenu}>
             <span className="material-icons">
               {openMenu ? "close" : "menu"}
