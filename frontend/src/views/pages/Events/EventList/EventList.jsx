ed09c4ed569ae6b99d8a448de4dc52a98c87d5f8
ammar <ammarnazmi44@gmail.com>
2024-03-18 06:01:03 +0800

    event

--- frontend/src/views/pages/Events/EventList/EventList.jsx
+++ frontend/src/views/pages/Events/EventList/EventList.jsx
@@ -1,8 +1,7 @@
 import { DUMMY_LIST_DATA } from "../../../../utils/data";
 import "./EventList.css";
-export default function EventList() {
-  let windowSize = window.matchMedia;
 
+export default function EventList() {
   return (
     <div className="container-fluid bg-white-event-list">
       <div className="container pt-5">
@@ -10,10 +9,21 @@ export default function EventList() {
           {DUMMY_LIST_DATA.map((item) => (
             <div className="col-3 mb-3 d-flex align-items-stretch">
               <div className="card">
-                <img className="card-img-top" src={item.image} alt="poster-image" />
-                <div className="card-content">
+                <img
+                  className="card-img-top"
+                  src={item.image}
+                  alt="poster-image"
+                />
+                <div class="card-body d-flex flex-column pb-0">
                   <h3 className="card-title">{item.title}</h3>
-                  <p>{windowSize}</p>
+                  <div className="location d-flex">
+                    <i className="bi bi-geo-alt-fill"></i>
+                    <p className="ps-2">Pantai Merdeka, Kedah</p>
+                  </div>
+                  <div className="card-footer mt-auto bg-transparent pb-0 d-flex event-date ps-0">
+                    <i class="bi bi-calendar3"></i>
+                    <p className="ps-2">24 march</p>
+                  </div>
                 </div>
               </div>
             </div>
